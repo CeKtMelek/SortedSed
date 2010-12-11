f8778f209d6227ab63eeefeb1699ca75372d11a2
Clemens <Robert@.(none)>
2010-12-11 14:22:00 +0100

    einfügen eines logger-Objekts und anlegen eines Sets aus DomainObjects in die Main-Klasse

--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -2,10 +2,23 @@ package at.jku.ce;
 
 public class Main {
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Set<DomainObject> objectSet = new HashSet<DomainObject>();
+
+		objectSet.add(new DomainObject("Helga"));
+		objectSet.add(new DomainObject("Walter"));
+		objectSet.add(new DomainObject("Hannah"));
+		objectSet.add(new DomainObject("Paul"));
+		objectSet.add(new DomainObject("Claudia"));
+
+		for (DomainObject domainObject : objectSet) {
+			LOGGER.debug(domainObject.id);
+		}
 	}
 
 }
