512c8c80feaaad46272f48169955a1ef342bdaaa
Clemens <Robert@.(none)>
2010-12-11 19:28:43 +0100

    Einfügen eines 2. Elements beim erstellen der DomainObject

--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -10,11 +10,11 @@ public class Main {
 	public static void main(String[] args) {
 		Set<DomainObject> objectSet = new HashSet<DomainObject>();
 
-		objectSet.add(new DomainObject("Helga"));
-		objectSet.add(new DomainObject("Walter"));
-		objectSet.add(new DomainObject("Hannah"));
-		objectSet.add(new DomainObject("Paul"));
-		objectSet.add(new DomainObject("Claudia"));
+		objectSet.add(new DomainObject("Helga", ""));
+		objectSet.add(new DomainObject("Walter", ""));
+		objectSet.add(new DomainObject("Hannah", ""));
+		objectSet.add(new DomainObject("Paul", ""));
+		objectSet.add(new DomainObject("Claudia", ""));
 
 		for (DomainObject domainObject : objectSet) {
 			LOGGER.debug(domainObject.id);
