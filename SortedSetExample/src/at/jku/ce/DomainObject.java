c85c5668f8c5fa4c6940631b7bc5aea39c937469
Clemens <Robert@.(none)>
2010-12-12 14:44:18 +0100

    einfügen des Comparable-interfaces mit der benötigten compareTo-Methode

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -1,6 +1,6 @@
 package at.jku.ce;
 
-public class DomainObject {
+public class DomainObject implements Comparable<DomainObject> {
 
 	private String uuid = UUID.randomUUID().toString();
 	private String name;
@@ -54,5 +54,13 @@ public class DomainObject {
 		} else {
 			return super.hashCode();
 		}
+	}
+
+	public int compareTo(DomainObject o) {
+		DomainObject domainObj = (DomainObject) o;
+		int domObjeComp = name.compareTo(domainObj.getName());
+
+		return ((domObjeComp == 0) ? uuid.compareTo(domainObj.getUuid()):
+			    domObjeComp);
 	}
 }
