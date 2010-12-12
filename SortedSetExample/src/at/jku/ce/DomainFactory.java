974d30df835f1f79f23b1e6f4bc66f897380ed9a
Artur <joe@hotmail.com>
2010-12-12 15:03:35 +0100

    Erstellen von Factory und Umstellen darauf.

---
+++ SortedSetExample/src/at/jku/ce/DomainFactory.java
@@ -0,0 +1,7 @@
+package at.jku.ce; 
+
+public class DomainFactory { 
+	public static DomainObject createDomainObject(String name, String comment) { 
+		return new DomainObject(name, comment); 
+	} 
+}
