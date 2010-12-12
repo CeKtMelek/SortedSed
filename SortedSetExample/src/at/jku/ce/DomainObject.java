4f21195d2536f786791872c38e9b5fc5ec5be844
Clemens <Robert@.(none)>
2010-12-12 15:08:20 +0100

    setzen der Konstruktoren auf protected

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -9,10 +9,10 @@ public class DomainObject {
 	/**
 	 * Default constructor
 	 */
-	public DomainObject() {
+	protected DomainObject() {
 		super();
 	}
-	public DomainObject(String name, String comment) {
+	protected DomainObject(String name, String comment) {
 		super();
 		this.name = name;
 	}
